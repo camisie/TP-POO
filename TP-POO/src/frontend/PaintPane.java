dab0a6d35843caa7fef14a8c99eb9e88eeb045a0
magflores <magflores@itba.edu.ar>
2022-07-07 22:31:02 -0300

    Alineación labels undo/redo

--- TP-POO/src/frontend/PaintPane.java
+++ TP-POO/src/frontend/PaintPane.java
@@ -110,30 +110,20 @@ public class PaintPane extends BorderPane {
 		topButtonsBox.setStyle("-fx-background-color: #999");
 		topButtonsBox.setPrefHeight(40);
 
-//		undoLabel.setMaxHeight(50);
-//		undoLabel.setMinWidth(320);
-//		undoLabel.setStyle("-fx-background-color: #939");
-//		undoLabel.setTextAlignment(TextAlignment.RIGHT);
-//		undoLabel.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
-//		redoLabel.setMaxHeight(50);
-//		redoLabel.setMinWidth(320);
-//		redoLabel.setStyle("-fx-background-color: #992");
-//		redoLabel.setTextAlignment(TextAlignment.RIGHT);
-//		redoLabel.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
-
 		//800
 		undoLabel.setPrefWidth(200);
-		undoLabel.setAlignment(Pos.CENTER_LEFT);
-		undoCounter.setPrefWidth(40);
+		undoLabel.setTextAlignment(TextAlignment.RIGHT);
+		undoLabel.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+		undoCounter.setPrefWidth(20);
 		undoCounter.setAlignment(Pos.CENTER_LEFT);
 		undoButton.setPrefWidth(100);
 		undoButton.setAlignment(Pos.CENTER);
 		redoButton.setPrefWidth(100);
 		redoButton.setAlignment(Pos.CENTER);
-		redoCounter.setPrefWidth(40);
+		redoCounter.setPrefWidth(20);
 		redoCounter.setAlignment(Pos.CENTER_RIGHT);
 		redoLabel.setPrefWidth(200);
-		redoLabel.setAlignment(Pos.CENTER_RIGHT);
+		redoLabel.setTextAlignment(TextAlignment.RIGHT);
 
 		canvas.setOnMousePressed(event -> {
 			startPoint = new Point(event.getX(), event.getY());
